74244a6a58bbd38684300aca7f4d2d0a92c2c196
Fabian beleño <fabianrobles321@outlook.com>
2025-05-18 14:21:11 -0500

    modifique

--- saludos.js
+++ saludos.js
@@ -1,14 +1,17 @@
+// funciones definidas
 function saludar(nombre) {
     return `Hola, ${nombre}`;
 }
-
+// funciones definidas
 function saludarHolamundo() {
     return "Hola, mundo!";
 }
 
+// Exportacion de la funcion metodo 1
 //module.exports.saludar = saludar;
 //module.exports.saludarHolamundo = saludarHolamundo;
 
+//Exportacionde la funcion metodo 2
 module.exports = {
     saludar: saludar,
     saludarHolamundo: saludarHolamundo
